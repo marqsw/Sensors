4bf5e091c2a5ea46e936dd2263770cea9604531d
Marquis <marqswong@gmail.com>
2024-10-25 18:41:13 +0100

    feat: setup VisionCamera

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,16 @@ import { ThemedView } from "@/components/ThemedView";
 import { ScrollView } from "react-native";
 import { HeaderTitle, useHeaderHeight } from "@react-navigation/elements";
 import Card from "@/components/Card";
+import { useCameraDevice, useCameraPermission } from "react-native-vision-camera";
 
 const gapSize = 25;
 
 export default function Home() {
   const headerHeight = useHeaderHeight();
+
+    // const device = useCameraDevice('back')
+    // const { hasPermission } = useCameraPermission()
+
 
   return (
     <ThemedView
