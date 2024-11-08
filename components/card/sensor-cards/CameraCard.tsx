7ff0cdc740590404158f250561f392a1c184d9ca
Marquis <marqswong@gmail.com>
2024-11-08 17:57:10 +0000

    feat: saving video after recording in CameraView

--- components/card/sensor-cards/CameraCard.tsx
+++ components/card/sensor-cards/CameraCard.tsx
@@ -1,30 +1,61 @@
 import { Button, Pressable, StyleSheet, View } from "react-native";
 import { ThemedText } from "../../ThemedText";
 import Card from "../Card";
-import { useContext, useEffect, useState } from "react";
-import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
+import { useContext, useEffect, useRef, useState } from "react";
+import {
+  CameraView,
+  CameraType,
+  useCameraPermissions,
+  Camera,
+} from "expo-camera";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withSpring,
 } from "react-native-reanimated";
-import { Ionicons } from "@expo/vector-icons";
 import IconButton from "@/components/IconButton";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { SelectionModeContext } from "@/components/context/recording/SelectionModeProvider";
+import { RecordingContext } from "@/components/context/recording/RecordingProvider";
+import { shareAsync } from 'expo-sharing';
+
 
 export default function CameraCard() {
+  let cameraRef = useRef<CameraView | null>(null);
+
   const textColor = useThemeColor({}, "text");
 
+  // permission
+  const [hasCameraPermission, setHasCameraPermission] = useState<boolean>();
+  const [hasMicrophonePermission, setHasMicrophonePermission] =
+    useState<boolean>();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] =
+    useState<boolean>();
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission =
+        await Camera.requestMicrophonePermissionsAsync();
+      const mediaLibraryPermission =
+
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMicrophonePermission(microphonePermission.status === "granted");
+    })();
+  }, []);
+
+  // camera
   const [facing, setFacing] = useState<CameraType>("back");
-  const [permission, requestPermission] = useCameraPermissions();
 
   const [enabletorch, setEnableTorch] = useState(false);
 
   const [expanded, setExpanded] = useState(false);
   const [selected, setSelected] = useState(false);
 
+  const recording = useContext(RecordingContext);
   const selectionMode = useContext(SelectionModeContext);
+
+  const [video, setVideo] = useState<{ uri: string } | undefined>(undefined);
 
   const animatedPadding = useSharedValue(0);
   const animatedViewStyle = useAnimatedStyle(() => {
@@ -37,38 +68,49 @@ export default function CameraCard() {
     animatedPadding.value = withSpring(expanded ? 5 : 0);
   }, [expanded]);
 
-  if (!permission) {
-    // Camera permissions are still loading.
+  useEffect(() => {
+    if (selected) {
+      if (recording) {
+        cameraRef.current?.recordAsync().then((recordedVideo) => {
+          setVideo(recordedVideo);
+        });
+      } else {
+        cameraRef.current?.stopRecording();
+      }
+    }
+  }, [recording]);
+
+  useEffect(() => {
+    if (video) {
+      shareAsync(video.uri)
+      setVideo(undefined)
+    }
+  }, [video]);
+
+  if (
+    hasCameraPermission === undefined ||
+    hasMicrophonePermission === undefined
+  ) {
     return (
       <Card
         expanded={expanded}
         setExpanded={setExpanded}
         selected={selected}
         setSelected={setSelected}
-      />
-    );
-  }
-
-  if (!permission.granted) {
-    <Card
-      expanded={expanded}
-      setExpanded={setExpanded}
-      selected={selected}
-      setSelected={setSelected}
-    >
-      <View
-        style={[
-          StyleSheet.absoluteFill,
-          {
-            alignItems: "center",
-            justifyContent: "center",
-          },
-        ]}
       >
-        <ThemedText>We need your permission to show the camera</ThemedText>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    </Card>;
+        <View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              alignItems: "center",
+              justifyContent: "center",
+            },
+          ]}
+        >
+          <ThemedText>We need your permission to show the camera</ThemedText>
+        </View>
+      </Card>
+    );
   }
 
   return (
@@ -85,7 +127,13 @@ export default function CameraCard() {
             { overflow: "hidden", borderRadius: 35, flex: 3 },
           ]}
         >
-          <CameraView facing={facing} style={StyleSheet.absoluteFill} />
+          <CameraView
+            mode="video"
+            ref={cameraRef}
+            videoQuality="2160p"
+            facing={facing}
+            style={StyleSheet.absoluteFill}
+          />
         </Animated.View>
         {expanded && (
           <View
@@ -94,20 +142,21 @@ export default function CameraCard() {
               margin: "5%",
               flexDirection: "row",
               justifyContent: "space-evenly",
+              alignItems: "center",
             }}
           >
             <IconButton
               iconName={enabletorch ? "flash" : "flash-outline"}
               iconColor={textColor}
-              buttonSize={80}
+              buttonSize={70}
               enabled={!selectionMode}
               handleOnPress={() => setEnableTorch((prev) => !prev)}
             />
             <IconButton
               iconName={"camera-reverse"}
               iconColor={textColor}
-              buttonSize={80}
-              enabled={!selectionMode}
+              buttonSize={70}
+              enabled={!(selectionMode || recording)}
               handleOnPress={() =>
                 setFacing((current) => (current === "back" ? "front" : "back"))
               }
@@ -118,3 +167,4 @@ export default function CameraCard() {
     </Card>
   );
 }
+
